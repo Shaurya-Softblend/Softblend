68194c3f31213a7e1139fa164dbbd57e8f11f94e
Shaurya Pratap Singh <shauryasinghad01@gmail.com>
2025-07-11 19:32:22 +0530

    header top and sidebar and content at same level

--- src/App.jsx
+++ src/App.jsx
@@ -24,19 +24,39 @@ function App() {
   return (
     // header  right to sidebar
 
-    <div className="w-full h-screen flex justify-start items-start ">
-      <Sidebar
+    // <div className="w-full h-screen flex justify-start items-start ">
+    //   <Sidebar
+    //     isMobileView={isMobileView}
+    //     mobileOpen={mobileOpen}
+    //     setMobileOpen={setMobileOpen}
+    //   />
+    //   <div className="flex flex-col w-full h-full overflow-y-auto">
+    //     <Navbar
+    //       isMobileView={isMobileView}
+    //       mobileOpen={mobileOpen}
+    //       setMobileOpen={setMobileOpen}
+    //     />
+    //     <div className="bg-[#253141] text-white w-full h-full px-10 py-1  ">
+    //       <Outlet />
+    //     </div>
+    //   </div>
+    // </div>
+
+    // header top
+    
+    <div className="w-full h-screen flex flex-col justify-start items-start ">
+      <Navbar
         isMobileView={isMobileView}
         mobileOpen={mobileOpen}
         setMobileOpen={setMobileOpen}
       />
-      <div className="flex flex-col w-full h-full overflow-y-auto">
-        <Navbar
+      <div className="flex justify-start items-start w-full h-full ">
+        <Sidebar
           isMobileView={isMobileView}
           mobileOpen={mobileOpen}
           setMobileOpen={setMobileOpen}
         />
-        <div className="bg-[#253141] text-white w-full h-full px-10 py-1  ">
+        <div className="bg-[#253141] text-white w-full h-full px-10 py-1 overflow-y-auto  ">
           <Outlet />
         </div>
       </div>
